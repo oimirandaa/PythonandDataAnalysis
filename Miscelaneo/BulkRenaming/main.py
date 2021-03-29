91afca9be7c4c783e716aeab8b7f045a459cc9ec
Oscar Miranda <oimirandaa08@gmail.com>
2021-03-29 01:23:23 -0600

    "Creating MasRenaming Program"

---
+++ Miscelaneo/BulkRenaming/main.py
@@ -0,0 +1,17 @@
+import os
+
+#This program renames all the files in the path variable. In this case, it renames the files as img.
+def main():
+    i = 1
+    path = ""
+    for filename in os.listdir(path):
+        my_dest = "img" + str(i) + ".jpg"
+        my_source = path + filename
+        my_dest = path + my_dest
+        os.rename(my_source, my_dest)
+        i += 1
+
+
+if __name__ == '__main__':
+    main()
+    
